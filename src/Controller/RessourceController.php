1da2fdf8d3f99ec104323cf0eea38af6af5f0056
ismailmag <71639431+ismailmag@users.noreply.github.com>
2020-12-07 19:42:44 +0100

    modification ressource

--- src/Controller/RessourceController.php
+++ src/Controller/RessourceController.php
@@ -80,4 +80,83 @@ class RessourceController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+        /**
+         * @Route("/", name="home")
+        */
+        public function listRessources() : Response
+        {
+            $repository = $this->getDoctrine()->getRepository(Ressource::class);
+            $ressources = $repository->findAll();
+
+
+            return $this->render('index.html.twig', [
+                'ressources' => $ressources,
+                
+            ]);
+            }
+
+        /**
+         * @Route("/editer-ressource/{id}", name="editRessource")
+        */
+        public function editRessources(Request $request, EntityManagerInterface $manager, Ressource $ressource): Response
+        {
+
+            $form = $this->createForm(RessourceType::class, $ressource);
+            $form->handleRequest($request);
+    
+    
+            if ($form->isSubmitted() && $form->isValid()) {    
+        
+                /** @var UploadedFile $ressourceFile */
+                $ressourceFile = $form->get('media')->getData();
+
+                // this condition is needed because the 'media' field is not required
+                // so the file must be processed only when a file is uploaded
+                if ($ressourceFile) {
+                    unlink($this->getParameter('medias_directory').'/'. $ressource->getMedia()); //ici je supprime le fichier
+
+                    $originalFilename = pathinfo($ressourceFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$ressourceFile->guessExtension();
+
+                    // Move the file to the directory where medias are stored
+                    try {
+                        $ressourceFile->move(
+                            $this->getParameter('medias_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        $this->addFlash(
+                            'notice',
+                            'Le media est pas telechargé !'
+                        );
+                            }
+
+                    // updates the 'ressourceFilename' property to store the file name
+                    // instead of its contents
+                    $ressource->setMedia($newFilename);
+                }
+
+
+                $manager->flush();
+    
+                $this->addFlash(
+                    'notice',
+                    'Le post a eté bien modifié !'
+                );
+                
+                return $this->redirectToRoute("home");
+    
+    
+            }
+    
+            return $this->render('ressource/addRessource.html.twig', [
+                'form' => $form->createView(),
+            ]);
+                    
+
+            }
+
 }
