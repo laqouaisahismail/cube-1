7bd2a7d55aa1d0af35cc607b69bb376ad0e1b435
jeremy PAQUOT <paquotjeremy@gmail.com>
2021-01-08 11:03:45 +0000

    Upload New File

---
+++ tests/membre.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="style.css" media="screen" type="text/css" /><?php
+session_start();
+if (!isset($_SESSION['login'])) {
+	header ('Location: index.php');
+	exit();
+}
+?>
+<html>
+<head>
+<title>Espace membre</title>
+</head>
+
+<body>
+Bienvenue <?php echo htmlentities(trim($_SESSION['login'])); ?> !<br />
+<a href="deconnexion.php">Déconnexion</a>
+</body>
+</html>
