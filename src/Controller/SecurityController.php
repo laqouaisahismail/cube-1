a694650d1622eebbf2816f87f5b2b7d62820c571
jeremy <jeremy>
2021-01-14 14:05:53 +0100

    logout ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Yaml\Exception\RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -54,14 +55,13 @@ class SecurityController extends AbstractController
      */
     public function login() {
         return $this->render('security/login.html.twig');    }
-
-        /**
+      
+     
+    /**
      * @Route("/logout",name="security_logout")
      */
     public function logout() {
 
-        return $this->render('security/logout.html.twig');    }
-        
-
-
-}
+        return $this->render('security/logout.html.twig');    } 
+    
+}   
