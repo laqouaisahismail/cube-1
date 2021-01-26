39b3260c0fafbbf37cd27847595d12b81b257872
ismailmag <71639431+ismailmag@users.noreply.github.com>
2021-01-26 18:37:35 +0100

    Style

--- migrations/Version20210126172235.php
+++ migrations/Version20210126172235.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210119184130 extends AbstractMigration
+final class Version20210126172235 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,13 +21,15 @@ final class Version20210119184130 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, content VARCHAR(255) NOT NULL, id_user INT NOT NULL, id_ressource INT NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('DROP TABLE admin');
+        $this->addSql('CREATE TABLE ressource (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(50) DEFAULT NULL, contenu VARCHAR(255) DEFAULT NULL, media VARCHAR(255) DEFAULT NULL, date DATETIME DEFAULT NULL, iduser INT DEFAULT NULL, categorie VARCHAR(50) DEFAULT NULL, statut VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(50) DEFAULT NULL, username VARCHAR(50) DEFAULT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(50) DEFAULT NULL, nom VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE ressource');
+        $this->addSql('DROP TABLE user');
     }
 }
